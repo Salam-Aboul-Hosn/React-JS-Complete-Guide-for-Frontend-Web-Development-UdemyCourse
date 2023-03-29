b27b73db63430494711302415e3684a0b18be3ce
Salam-Aboul-Hosn <salamaboulhosn581@gmail.com>
2023-03-29 03:19:23 -0700

    Working on Javascript

--- Calculator App/index.js
+++ Calculator App/index.js
@@ -10,15 +10,21 @@ content.appendChild(document.createTextNode("helllo there"))
 buttons.forEach(button => {
     button.addEventListener("click", function(event) {
         let value = event.target.textContent;
-        if(value !== "C") {
+        console.log(value)
+        if(value === "=") {
+            let newListElement = document.createElement("span")
+            let answer = calculate(value, value)
+            let textNode = document.createTextNode(answer)
+            newListElement.appendChild(textNode)
+            whiteSpace.appendChild(newListElement)
+
+        } else if(value !== "C") {
             let newListElement = document.createElement("span")
             let textNode = document.createTextNode(value)
             newListElement.appendChild(textNode)
             whiteSpace.appendChild(newListElement)
             newListElement.style.padding = "10px";
             newListElement.style.fontSize = "25px";
-        } else if(value === "=") {
-            alert("worked")
         } else {
             let parentElement = document.getElementById("white-space");
             while (parentElement.firstChild) {
@@ -27,3 +33,15 @@ buttons.forEach(button => {
         }
     })
 })
+
+function calculate(num1, num2, operator) {
+    if(operator === "+") {
+        return num1 + num2
+    } else if (operator === "-") {
+        return num1 - num2;
+    } else if (operator === "*") {
+        return num1 * num2
+    } else if(operator === "/") {
+        return num1 / num2
+    }
+}
